da7e33891d2fbffc5511a1a6677d899bdf72fc9f
Andy Dirnberger <dirn@dirnonline.com>
2015-05-27 10:45:11 -0400

    Remove unused test fixtures
    These fixtures were leftover from the old namespace package approach
    that was removed in 9a99bba.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,26 +6,6 @@ from henson.registry import Registry
 
 
 @pytest.fixture
-def modules_tmpdir(tmpdir, monkeypatch):
-    """Add a temporary directory for modules to sys.path."""
-    tmp = tmpdir.mkdir('tmp_modules')
-    monkeypatch.syspath_prepend(str(tmp))
-    return tmp
-
-
-@pytest.fixture
-def mock_service(modules_tmpdir):
-    """Create a package for a fake service."""
-    service = modules_tmpdir.mkdir('mock_service')
-    service.join('settings.py').write('SERVICE_SETTING = 1')
-    service.join('process.py').write('def run(app, message): return message')
-
-    # Add a couple of other modules that can be used for testing errors.
-    service.join('bad_import.py').write('import not_a_real_module')
-    service.join('type_error.py').write('1 + "a"')
-
-
-@pytest.fixture
 def registry():
     """Create an application registry."""
     return Registry()
