ae8ab73185bcb6ad0468b1d8faf079f897dea737
Andy Dirnberger <dirn@dirnonline.com>
2015-06-08 17:50:24 -0400

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Henson',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['tests']),
     tests_require=[
         'tox',
