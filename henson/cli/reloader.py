b2d2e5ce781f36d0f8bdb94a7d4533275b72ed3b
Jon Banafato <jon@jonafato.com>
2015-08-10 13:20:11 -0400

    Document arguments to ApplicationReloaderRunner and FunctionCallingEventHandler

--- henson/cli/reloader.py
+++ henson/cli/reloader.py
@@ -10,7 +10,14 @@ __all__ = ('FunctionCallingEventHandler', 'ApplicationReloaderRunner')
 
 class ApplicationReloaderRunner:
 
-    """Wrap StoppableThread and restart when required."""
+    """Wrap StoppableThread and restart when required.
+
+    Args:
+        module (module): the module that should be reloaded after each
+          change is detected
+        app_name (str): the name of the :class:`~Henson.Application`
+          instance that should be run
+    """
 
     def __init__(self, module, app_name):
         """Store references to the module and app name to restart later."""
@@ -34,10 +41,30 @@ class ApplicationReloaderRunner:
 
 class FunctionCallingEventHandler(PatternMatchingEventHandler):
 
-    """Restart a Henson application in response to filesystem events."""
+    """Restart a Henson application in response to filesystem events.
 
-    def __init__(self, target, args=(), kwargs={}, patterns=None,
-                 ignore_patterns=None, ignore_directories=None,
+    Args:
+        target (callable): the function to be run on every event emitted
+          by the :class:`~watchdog.observers.Observer`.
+        args (Optional[iterable]): positional arguments to be passed to
+          the target function.
+        kwargs (Optional[dict]): keyword arguments to be passed to the
+          target function.
+        patterns (Optional[iterable]): a list of string patterns.
+          Filenames matching these patterns will cause events to fire,
+          others will be ignored. Defaults to ``['*']``.
+        ignore_patterns (Optional[iterable]): a list of string patterns.
+          Filenames matching these patterns will be explicitly ignored
+          for events. Mutually exclusive with ``patterns``, defaults to
+          ``[]``.
+        ignore_directories (Optional[bool]): whether or not to ignore
+          directory-level changes. Defaults to ``False``.
+        case_sensitive (Optional[bool]): whether or not patterns are
+          case-sensitive. Defaults to ``False``.
+    """
+
+    def __init__(self, target, args=(), kwargs=None, patterns=None,
+                 ignore_patterns=None, ignore_directories=False,
                  case_sensitive=False):
         """Store the target function to be called on events."""
         super().__init__(
@@ -46,6 +73,9 @@ class FunctionCallingEventHandler(PatternMatchingEventHandler):
             ignore_directories=ignore_directories,
             case_sensitive=case_sensitive,
         )
+
+        if kwargs is None:
+            kwargs = {}
         self.target = target
         self.args = args
         self.kwargs = kwargs
