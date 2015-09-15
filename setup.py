d0c68b614c45dd9289ea0399c2b5276985d9cc78
Jon Banafato <jon@jonafato.com>
2015-09-15 12:06:02 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -16,4 +16,15 @@ setup(
         [console_scripts]
         henson=henson.cli:cli
     ''',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ]
 )
