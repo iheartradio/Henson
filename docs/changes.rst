ca80615a6cec7e01202f9884631bac37e43297f7
Andy Dirnberger <dirn@dirnonline.com>
2015-10-23 12:10:03 -0400

    Bump version 0.5.0

--- docs/changes.rst
+++ docs/changes.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+Version 0.5.0
+-------------
+
+Released TBD
+
 Version 0.4.0
 -------------
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Henson',
-    version='0.4.0',
+    version='0.5.0',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         # TODO: determine minimum versions for requirements
