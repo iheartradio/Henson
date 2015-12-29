fe9025cf4a6e12158b0f51b31a69a4ba30307cbc
Jon Banafato <jon@jonafato.com>
2015-12-29 14:26:18 -0500

    Move location of version information
    Currently, Henson must be installed to provide version information. This
    is problematic when developing Henson (i.e. populating
    ``henson.__version__`` causes errors). Version information is still
    stored in only one location, but it it being moved to within the package
    to address this issue.

--- henson/__init__.py
+++ henson/__init__.py
@@ -1,8 +1,6 @@
 """Henson."""
 
-from pkg_resources import get_distribution
-
 from .base import Application  # NOQA
 from .extensions import Extension  # NOQA
 
-__version__ = get_distribution(__package__).version
+__version__ = '0.5.0'
